121db70bcce951f02cf704712de813d7b4dd39fa
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-09-01 14:19:41 -0600

    Borrando clases wather y agregando clase Value

---
+++ Laboratorio1/Laboratorio1/Models/Value.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio1.Models
+{
+    public class Value : IComparable
+    {
+        public string name { get; set; }
+        public int year { get; set; }
+        public string directed_by { get; set; }
+        public string stars { get; set; }
+        public bool genere { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
