af9ac48bf46c538ae0203ddf0f3b23d44b9c1e4a
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-09-08 21:31:21 -0600

    Separando Serie I de II ya gregando singleton

---
+++ Laboratorio1/Multi-Tree/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Multi_Tree;
+
+namespace Multi_Tree
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
